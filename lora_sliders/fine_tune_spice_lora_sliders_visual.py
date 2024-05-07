63a478101efc9b8ff27ccd3404e8b75f65c40042
noamatia <noamatia15@gmail.com>
2024-05-07 12:03:44 +0000

    new _datasets

--- lora_sliders/fine_tune_spice_lora_sliders_visual.py
+++ lora_sliders/fine_tune_spice_lora_sliders_visual.py
@@ -5,7 +5,10 @@ from shap_e.diffusion.gaussian_diffusion import mean_flat
 
 
 name, paths = build_name_and_paths()
-wandb.init(project=args.wandb_project, name=name, config=vars(args))
+if args.test:
+    os.makedirs(paths['samples'], exist_ok=True)
+    os.makedirs(paths['models'], exist_ok=True)
+    wandb.init(project=args.wandb_project, name=name, config=vars(args))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: {device}")
@@ -31,7 +34,8 @@ model_kwargs_high_train_list =  build_model_kwargs_list(prompt_high, latents_tra
 model_kwargs_high_test_list =  build_model_kwargs_list(prompt_high, latents_test, "test", "high", latents_low_test)
 model_kwargs_low_train_list =  build_model_kwargs_list(prompt_low, latents_train, "train", "low", latents_high_train)
 model_kwargs_low_test_list =  build_model_kwargs_list(prompt_low, latents_test, "test", "low", latents_high_test)
-decode_and_export_test_conditions(model_kwargs_high_test_list, model_kwargs_low_test_list, paths, cameras, xm)
+if args.test:
+    decode_and_export_test_conditions(model_kwargs_high_test_list, model_kwargs_low_test_list, paths, cameras, xm)
 masks_train = build_masks(latents_high_train, latents_low_train)
 # masks_test = build_masks(latents_high_test, latents_low_test)
 # sanity_check(latents_high_train, latents_low_train, masks_train, cameras, xm, 'train')
@@ -39,19 +43,18 @@ masks_train = build_masks(latents_high_train, latents_low_train)
 
 sigma_max = 160
 x_T_test = torch.randn((1, model.d_latent), device=device).expand(1, -1) * sigma_max
-torch.save(x_T_test, paths['x_T_test'])
 eps_test = torch.randn_like(x_T_test)
-torch.save(eps_test, paths['eps_test'])
+if args.test:
+    torch.save(x_T_test, paths['x_T_test'])
+    torch.save(eps_test, paths['eps_test'])
 
 def train_step(scale: int, batch: torch.Tensor, timesteps: torch.Tensor, model_kwargs: dict, noise: torch.Tensor) -> int:
-    try:
-        network.set_lora_slider(scale)
-        with network:
-            losses = diffusion.training_losses(model, batch, timesteps, model_kwargs, noise)
-        loss = losses['loss'] / args.grad_acc_steps
-        return loss, losses['output']
-    except:
-        pass
+    network.set_lora_slider(scale)
+    with network:
+        losses = diffusion.training_losses(model, batch, timesteps, model_kwargs, noise)
+    loss = losses['loss'] / args.grad_acc_steps
+    return loss, losses['output']
+
         
 def test_step(scale: int, epoch: int, log_data: dict, high_or_low: str, model_kwargs_test_list: list):
     for test_model_kwargs in tqdm(model_kwargs_test_list, total=len(model_kwargs_test_list), desc=f"Testing {high_or_low} epoch {epoch}"):
@@ -71,7 +74,7 @@ def test_step(scale: int, epoch: int, log_data: dict, high_or_low: str, model_kw
         for i, test_latent in enumerate(test_latents):
             uid = test_model_kwargs['uid'][i]
             uid_output_dir = os.path.join(paths['samples'], uid, high_or_low)
-            log_data[f"output_{uid}_{high_or_low}"] = wandb.Video(decode_and_export(test_latent, uid_output_dir, f'output_{epoch}.gif', cameras, xm))
+            log_data[f"{high_or_low}_output_{uid}"] = wandb.Video(decode_and_export(test_latent, uid_output_dir, f'output_{epoch}.gif', cameras, xm))
 
 grad_acc_step = 0
 pbar = tqdm(range(args.epochs))
@@ -96,14 +99,15 @@ for i in pbar:
         if grad_acc_step % args.grad_acc_steps == 0:
             optimizer.step()
             optimizer.zero_grad()
-            
-    log_data = {"loss_high": loss_for_epoch_high, "loss_low": loss_for_epoch_low, "loss_masked_diff": loss_for_epoch_masked_diff}
-    avg_loss_for_epoch = (loss_for_epoch_high + loss_for_epoch_low + loss_for_epoch_masked_diff) / 3
-    pbar.set_description(f"loss: {avg_loss_for_epoch:.4f}")
-    if i % args.test_steps == 0:
-        test_step(scale_high, i, log_data, "high", model_kwargs_high_test_list)
-        test_step(scale_low, i, log_data, "low", model_kwargs_low_test_list)
-        network.save_weights(os.path.join(paths['models'], f'model_{i}.pt'))
-    wandb.log(log_data)
-network.save_weights(os.path.join(paths['models'], 'model_final.pt'))
+    if args.test:  
+        log_data = {"loss_high": loss_for_epoch_high, "loss_low": loss_for_epoch_low, "loss_masked_diff": loss_for_epoch_masked_diff}
+        avg_loss_for_epoch = (loss_for_epoch_high + loss_for_epoch_low + loss_for_epoch_masked_diff) / 3
+        pbar.set_description(f"loss: {avg_loss_for_epoch:.4f}")
+        if i % args.test_steps == 0:
+            test_step(scale_high, i, log_data, "high", model_kwargs_high_test_list)
+            test_step(scale_low, i, log_data, "low", model_kwargs_low_test_list)
+            network.save_weights(os.path.join(paths['models'], f'model_{i}.pt'))
+        wandb.log(log_data)
+if args.test: 
+    network.save_weights(os.path.join(paths['models'], 'model_final.pt'))
     
--- lora_sliders/utils.py
+++ lora_sliders/utils.py
@@ -1,19 +1,30 @@
 import os
 import sys
+spice = True
+if spice:
+    sys.path.insert(0, os.path.join(os.path.abspath(os.getcwd()), "spic_e", "shap_e"))
+os.environ["WANDB_API_KEY"] = "7b14a62f11dc360ce036cf59b53df0c12cd87f5a"
+import json
+import wandb
+import torch
+import random
 import argparse
+import numpy as np
+from tqdm import tqdm
+from datetime import datetime
+from lora import LoRANetwork
+from diffusers.utils import export_to_gif
+from shap_e.util.notebooks import decode_latent_images
+from shap_e.models.download import load_model, load_config
+from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--name", type=str, default='spice_with_prompts_masking_percentile_90_beta_0_5', help="Name of the run.")
+parser.add_argument("--name", type=str, default='spice_with_prompts_masking_percentile_90_beta_0_75', help="Name of the run.")
 parser.add_argument("--alpha", type=float, default=1.0, help="Alpha of LoRA.")
 parser.add_argument("--rank", type=int, default=4, help="Rank of LoRA.")
-parser.add_argument("--slider_name", type=str, default="armsslider", help="Name of the slider.")
-# parser.add_argument("--slider_name", type=str, default="backslider", help="Name of the slider.")
+parser.add_argument("--obj", type=str, default="chair", help="Name of the slider.")
+parser.add_argument("--prompt_index", type=int, default=0, help="Rank of LoRA.")
 parser.add_argument('--epochs', type=int, default=1000, help='number of epochs')
-parser.add_argument('--data_dir', type=str, default='/home/noamatia/repos/shape_lora_sliders/lora_sliders/datasets/arms', help='data directory')
-# parser.add_argument('--data_dir', type=str, default='/home/noamatia/repos/shape_lora_sliders/lora_sliders/datasets/back', help='data directory')
-parser.add_argument('--prompts', type=str, default='without armrests, with armrests', help='prompts for generation')
-# parser.add_argument('--prompts', type=str, default='with a spindle backrest, with a solid backrest', help='prompts for generation')
-parser.add_argument( "--folders", type=str, default='withoutarms/latents, witharms/latents', help="folders with different attribute-scaled images")
-# parser.add_argument( "--folders", type=str, default='spindle/latents, solid/latents', help="folders with different attribute-scaled images")
 parser.add_argument( "--scales", type=str, default = '-1, 1', help  ="scales for different attribute-scaled images")
 parser.add_argument( "--batch_size", type=int, default=4, help="batch size")
 parser.add_argument( "--grad_acc_steps", type=int, default=16, help="max timesteps")
@@ -26,29 +37,16 @@ parser.add_argument( "--render_mode", type=str, default="nerf", help="render mod
 parser.add_argument('--size', type=int, default=160, help='images size')
 parser.add_argument('--test_steps', type=int, default=50, help='test steps')
 parser.add_argument('--masking_diff_percentile', type=int, default=90, help='masking diff percentile')
-parser.add_argument( "--beta", type=float, default=0.5, help="beta")
-parser.add_argument("--uid_to_object", type=str, default='uid_to_object', help="uid to object")
-parser.add_argument("--spice_model_path", type=str, default="/scratch/noam/spice_models/semantic_editing_chair/best_model.pt", help="path to spice model")
-parser.add_argument("--gpu", type=str, default="2", help="gpu id")
+parser.add_argument("--beta", type=float, default=0.75, help="beta")
+parser.add_argument('--test', action='store_true', help='test and use wandb')
 args = parser.parse_args()
-if args.spice_model_path:
-    sys.path.insert(0, os.path.join(os.path.abspath(os.getcwd()), "spic_e"))
-os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-os.environ["WANDB_API_KEY"] = "7b14a62f11dc360ce036cf59b53df0c12cd87f5a"
 
-import json
-import wandb
-import torch
-import random
-import numpy as np
-from tqdm import tqdm
-from datetime import datetime
-from lora_sliders.lora import LoRANetwork
-from diffusers.utils import export_to_gif
-from shap_e.util.notebooks import decode_latent_images
-from shap_e.models.download import load_model, load_config
-from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
-
+with open(os.path.join('/storage/noamatia/repos/shape_lora_sliders/lora_sliders/llama3', f'{args.obj}.json'), 'r') as f:
+    pairs = json.load(f)
+pair = pairs[args.prompt_index]
+low_prompt, high_prompt = pair[0], pair[1]
+slider_name = f'{low_prompt.replace(" ", "_")}_{high_prompt.replace(" ", "_")}'
+data_dir = f'/storage/noamatia/shape_lora_sliders_datasets/{args.obj}/{slider_name}'
 
 def parse_arg(arg: str, is_int: bool = False) -> np.ndarray:
     arg = arg.split(',')
@@ -59,27 +57,25 @@ def parse_arg(arg: str, is_int: bool = False) -> np.ndarray:
 
 def build_name_and_paths() -> tuple:
     paths = {}
-    name = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}_{args.slider_name}_{args.name}"
-    output_dir = os.path.join('/scratch/noam/shape_lora_sliders', name)
+    name = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}_{slider_name}_{args.name}"
+    output_dir = os.path.join('/storage/noamatia/shape_lora_sliders', name)
     paths['samples'] = os.path.join(output_dir, 'samples')
-    os.makedirs(paths['samples'], exist_ok=True)
     paths['x_T_test'] = os.path.join(output_dir, 'x_T_test.pt')
     paths['eps_test'] = os.path.join(output_dir, 'eps_test.pt')
     paths['models'] = os.path.join(output_dir, 'models')
-    os.makedirs(paths['models'], exist_ok=True)
     return name, paths
 
 def build_models(device: torch.device) -> tuple:
-    if args.spice_model_path:
+    if spice:
         cache_dir = os.path.join(os.path.abspath(os.getcwd()), "spic_e_model_cache")
     else:
         cache_dir = os.path.join(os.path.abspath(os.getcwd()), "shap_e_model_cache")
     xm = load_model('transmitter', device=device, cache_dir=cache_dir)
     model = load_model('text300M', device=device, cache_dir=cache_dir)
-    if args.spice_model_path:
+    if spice:
         model.wrapped.backbone.make_ctrl_layers()
         model.wrapped.set_up_controlnet_cond()
-        model.load_state_dict(torch.load(args.spice_model_path))
+        model.load_state_dict(torch.load(os.path.join('/storage/noamatia/repos/shape_lora_sliders/lora_sliders/spice_models', f'{args.obj}.pt')))
     model.wrapped.cond_drop_prob = args.cond_drop_prob
     model.freeze_all_parameters()
     network = LoRANetwork(model.wrapped, args.rank, args.alpha).to(device)
@@ -90,29 +86,32 @@ def build_models(device: torch.device) -> tuple:
     return xm, model, network, diffusion, optimizer
 
 def build_folders_prompts_and_scales() -> tuple:
-    folders = parse_arg(args.folders)
-    prompts = parse_arg(args.prompts)
+    # folders = parse_arg(args.folders)
+    # prompts = parse_arg(args.prompts)
     scales = parse_arg(args.scales, is_int=True)
-    assert folders.shape[0] == scales.shape[0], "The number of folders and scales must be the same."
-    assert prompts.shape[0] == scales.shape[0], "The number of prompts and scales must be the same."
+    # assert folders.shape[0] == scales.shape[0], "The number of folders and scales must be the same."
+    # assert prompts.shape[0] == scales.shape[0], "The number of prompts and scales must be the same."
     assert scales.shape[0] == 2, "Currently the number of scales must be 2."
     scale_high = abs(random.choice(list(scales)))
     scale_low = -scale_high
-    folder_high = folders[scales==scale_high][0]
-    folder_low = folders[scales==scale_low][0]
-    prompt_high = prompts[scales==scale_high][0]
-    prompt_low = prompts[scales==scale_low][0]
-    return folder_high, folder_low, prompt_high, prompt_low, scale_high, scale_low
+    # folder_high = folders[scales==scale_high][0]
+    # folder_low = folders[scales==scale_low][0]
+    # prompt_high = prompts[scales==scale_high][0]
+    # prompt_low = prompts[scales==scale_low][0]
+    folder_high = f'{high_prompt.replace(" ", "_")}/latents'
+    folder_low = f'{low_prompt.replace(" ", "_")}/latents'
+    return folder_high, folder_low, high_prompt, low_prompt, scale_high, scale_low
 
 def build_latents(test_or_train: str, folder:str ) -> list:
-    latents = os.listdir(os.path.join(args.data_dir, test_or_train, folder))
+    latents = os.listdir(os.path.join(data_dir, test_or_train, folder))
     latents = [latent for latent in latents if '.pt' in latent]
-    wandb.config[f"{test_or_train}_dataset_size"] =  len(latents)
-    wandb.config[f"{test_or_train}_latents_ids"] =  latents
+    if args.test:
+        wandb.config[f"{test_or_train}_dataset_size"] =  len(latents)
+        wandb.config[f"{test_or_train}_latents_ids"] =  latents
     return latents
 
 def load_latents(test_or_train: str, folder: str, device: torch.device, latents: list) -> list:
-    lats = [torch.load(os.path.join(args.data_dir, test_or_train, folder, latent)).to(device) for latent in latents]
+    lats = [torch.load(os.path.join(data_dir, test_or_train, folder, latent)).to(device) for latent in latents]
     if len(lats) % args.batch_size != 0:
         lats += [lats[i] for i in range(args.batch_size - (len(lats) % args.batch_size))]
     lats = [lats[i:i + args.batch_size] for i in range(0, len(lats), args.batch_size)]
@@ -120,14 +119,15 @@ def load_latents(test_or_train: str, folder: str, device: torch.device, latents:
     return lats
 
 def build_model_kwargs_list(prompt: str, latents: list, test_or_train: str, low_or_high: str, loaded_latents: list = None) -> tuple:
-    if args.uid_to_object:
+    uid_to_object = False
+    if uid_to_object:
         assert prompt != '', "Prompt must be provided if uid_to_object is provided."
-        with open(os.path.join(args.data_dir, test_or_train, f'{args.uid_to_object}.json'), 'r') as f:
+        with open(os.path.join(data_dir, test_or_train, f'{args.uid_to_object}.json'), 'r') as f:
             uid_to_object = json.load(f)
         prompts = [uid_to_object[latent.split('.')[0]] + ' ' + prompt for latent in latents]
     else:
-        assert prompt == '', "Prompt must be empty if uid_to_object is not provided."
-        prompts = ['' for _ in latents]
+        # assert prompt == '', "Prompt must be empty if uid_to_object is not provided."
+        prompts = [prompt for _ in latents]
     uids = [latent.split('.')[0] for latent in latents]
     if len(prompts) % args.batch_size != 0:
         prompts += [prompts[i] for i in range(args.batch_size - (len(prompts) % args.batch_size))]
@@ -143,7 +143,8 @@ def build_model_kwargs_list(prompt: str, latents: list, test_or_train: str, low_
             import pdb; pdb.set_trace()
         assert len(prompts[-1]) == len(loaded_latents[-1]), "The number of prompts and latents must be the same."
         model_kwargs = [dict(texts=prompts[i], cond=loaded_latents[i], uid=uids[i]) for i in range(len(prompts))]
-    wandb.config[f"model_kwargs_{test_or_train}_{low_or_high}"] = model_kwargs
+    if args.test:
+        wandb.config[f"model_kwargs_{test_or_train}_{low_or_high}"] = model_kwargs
     return model_kwargs
 
 def build_masks(latents_high: list, latents_low: list) -> list:
@@ -171,6 +172,7 @@ def sanity_check(latents_high, latents_low, masks, cameras, xm, test_or_train):
             decode_and_export(masked_latent[j], output_dir, f'{test_or_train}_{i}_{j}_masked.gif', cameras, xm)
 
 def decode_and_export_test_conditions(model_kwargs_high_test_list, model_kwargs_low_test_list, paths, cameras, xm):
+    log_data = {}
     for model_kwargs_high_test, model_kwargs_low_test in tqdm(zip(model_kwargs_high_test_list, model_kwargs_low_test_list), 
                                                               total=len(model_kwargs_high_test_list), 
                                                               desc='Decoding and exporting test conditions'):
@@ -182,6 +184,6 @@ def decode_and_export_test_conditions(model_kwargs_high_test_list, model_kwargs_
             uid_output_dir_low = os.path.join(paths['samples'], uid_test_low, 'low')
             os.makedirs(uid_output_dir_high, exist_ok=True)
             os.makedirs(uid_output_dir_low, exist_ok=True)
-            # decode_and_export(model_kwargs_high_test['cond'][i], uid_output_dir_high, f'condition.gif', cameras, xm)
-            # decode_and_export(model_kwargs_low_test['cond'][i], uid_output_dir_low, f'condition.gif', cameras, xm)
-            
+            log_data[f"high_condition_{uid_test_high}"] = wandb.Video(decode_and_export(model_kwargs_high_test['cond'][i], uid_output_dir_high, f'condition.gif', cameras, xm))
+            log_data[f"low_condition_{uid_test_low}"] = wandb.Video(decode_and_export(model_kwargs_low_test['cond'][i], uid_output_dir_low, f'condition.gif', cameras, xm))
+    wandb.log(log_data)
